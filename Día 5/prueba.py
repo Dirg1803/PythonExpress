585a13650ee91943542a81ed28da6f84152ffaf4
Diego Rosales <diego.rosales@ibm.com>
2022-05-07 00:11:45 -0500

    Se agrega parte de los ejercicios del día 5, nos quedamos en ejemplo función

--- Día 5/prueba.py
+++ Día 5/prueba.py
@@ -1,9 +1,22 @@
+def suma_menores(suma):
+    sum=0
+    for n in suma:
+        if n in range(0, 100):
+            sum += n
+    return sum
 
-def invertir_palabra(nombre):
-    a=list(nombre)
-    print(reversed(a))
 
-palabra="python"
-invertir_palabra(palabra)
-    
-    
+lista_numeros = [1, 2, 3, 4, 5,700,100,0,1]
+print(suma_menores(lista_numeros))
+
+
+def cantidad_pares(pares):
+    cantidad = 0
+    for n in pares:
+        if n % 2 == 0:
+            cantidad += 1
+    return cantidad
+
+
+lista_numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(cantidad_pares(lista_numeros))
